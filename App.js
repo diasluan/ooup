ec9b7de62f7d03585d62d5cf2f571c39b55f727a
Dias, Luan <diasluan@amazon.com>
2021-01-07 02:31:11 +0100

    add aws amplify and graphql api

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Amplify from 'aws-amplify'
+import awsconfig from './aws-exports'
+Amplify.configure(awsconfig)
+
 export default function App() {
   return (
     <View style={styles.container}>
