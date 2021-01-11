e25d2365d7f0b565f7f762fa89227288aaba2c36
Dias, Luan <diasluan@amazon.com>
2021-01-12 00:31:12 +0100

    change screens navigation

--- navigation/Screens.js
+++ navigation/Screens.js
@@ -1,12 +1,23 @@
 import React from "react"
 
 import { createStackNavigator } from "@react-navigation/stack"
+import { createDrawerNavigator } from "@react-navigation/drawer"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 
 import Onboarding from "../screens/Onboarding"
+import Register from "../screens/Register"
+import Login from "../screens/Login"
+
+
+import CustomDrawerContent from "./Menu"
+
+const { width } = Dimensions.get("screen")
 
 const Stack = createStackNavigator()
+const Drawer = createDrawerNavigator()
+const Tab = createBottomTabNavigator()
 
-export default function OnboardingStack(props) {
+const OnboardingStack = (props) => {
   return (
     <Stack.Navigator mode="card" headerMode="none">
       <Stack.Screen
@@ -19,3 +30,42 @@ export default function OnboardingStack(props) {
     </Stack.Navigator>
   )
 }
+
+const AppStack = (props) => {
+  return (
+    <Drawer.Navigator
+      style={{ flex: 1 }}
+      drawerContent={props => <CustomDrawerContent {...props} />}
+      drawerStyle={{
+        backgroundColor: "white",
+        width: width * 0.8
+      }}
+      drawerContentOptions={{
+        activeTintcolor: "white",
+        inactiveTintColor: "#000",
+        activeBackgroundColor: "transparent",
+        itemStyle: {
+          width: width * 0.75,
+          backgroundColor: "transparent",
+          paddingVertical: 16,
+          paddingHorizonal: 12,
+          justifyContent: "center",
+          alignContent: "center",
+          alignItems: "center",
+          overflow: "hidden"
+        },
+        labelStyle: {
+          fontSize: 18,
+          marginLeft: 12,
+          fontWeight: "normal"
+        }
+      }}
+      initialRouteName="Register"
+    >
+      <Drawer.Screen name="Register" component={Register} />
+      <Drawer.Screen name="Login" component={Login} />
+    </Drawer.Navigator>
+  )
+}
+
+export default OnboardingStack
