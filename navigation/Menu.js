3a1fc07c38946285782e66cde6557978e80ba7dc
Dias, Luan <diasluan@amazon.com>
2021-02-14 12:32:11 +0100

    add navigation menu

---
+++ navigation/Menu.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { ScrollView, StyleSheet, Dimensions, Image } from 'react-native'
+import { Block, theme } from 'galio-framework'
+import Images from '../constants/Images'
+import { DrawerItem as DrawerCustomItem } from '../components/index'
+
+const { width } = Dimensions.get('screen')
+
+const CustomDrawerContent = ({ drawerPosition, navigation, profile, focused, state, ...rest }) => {
+  const screens = [
+    'Register',
+    'Login'
+  ]
+  return (
+    <Block
+      style={styles.container}
+      forceInset={{ top: 'always', horizontal: 'never' }}
+    >
+      <Block flex={0.06} style={styles.header}>
+        <Image styles={styles.logo} source={Images.Logo} />
+      </Block>
+      <Block flex style={{ paddingLeft: 8, paddingRight: 14 }}>
+        <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
+          {screens.map((item, index) => {
+              return (
+                <DrawerCustomItem
+                  title={item}
+                  key={index}
+                  navigation={navigation}
+                  focused={state.index === index ? true : false}
+                />
+              )
+            })}
+        </ScrollView>
+      </Block>
+    </Block>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  header: {
+    paddingHorizontal: 28,
+    paddingBottom: theme.SIZES.BASE,
+    paddingTop: theme.SIZES.BASE * 3,
+    justifyContent: 'center'
+  }
+})
+
+export default CustomDrawerContent
